cf931dedb721d109ecc5b621520384ec81eb2c18
rbverissimo <rverissimo.dev@gmail.com>
2023-11-01 19:59:37 -0300

    Tentando ajustar o header

--- resources/views/components/header-global.blade.php
+++ resources/views/components/header-global.blade.php
@@ -1,27 +1,8 @@
 <header class="header-global">
-    <div class="container-menu">
-        <div class="menu-icone">
-            <div class="barra"></div>
-            <div class="barra"></div>
-            <div class="barra"></div>
-        </div>
-        <div class="dropdown-menu">
-            <ul>
-                <li><a href="{{route('site.home')}}">Home</a></li>
-                <li><a href="{{route('site.painel')}}">Painel</a></li>
-            </ul>
-        </div>
+    <div>
+        <ul>
+            <li><a href="{{route('site.home')}}">Home</a></li>
+            <li><a href="{{route('site.painel')}}">Painel</a></li>
+        </ul>
     </div>
 </header>
-
-<script>
-    document.addEventListener('DOMContentLoaded', function(){
-    const menuIcone = document.querySelector('.menu-icone');
-    const dropdownMenu = document.querySelector('.dropdown-menu');
-
-    menuIcone.addEventListener('click', () =>{
-        dropdownMenu.classList.toggle('menu-open');
-    });
-
-});
-</script>
