0f74b6c0af0bf6e0399b33ff57bc6312c0245c1f
rbverissimo <rverissimo.dev@gmail.com>
2023-11-02 21:16:43 -0300

    Melhorando a responsividade do app

--- resources/views/components/header-global.blade.php
+++ resources/views/components/header-global.blade.php
@@ -1,11 +1,10 @@
 <header class="header-global">
     <div class="header-logo">
-
+        <div class="div-header-logo">
+            <a class="header-link-logo" href="{{route('site.home')}}">Nome do produto</a>
+        </div>
     </div>
     <div class="container-links-header">
-        <div class="div-header-link">
-            <a class="header-link" href="{{route('site.home')}}">Home</a>
-        </div>
         <div class="div-header-link">
             <a class="header-link" href="{{route('site.painel')}}">Blog</a>
         </div>
