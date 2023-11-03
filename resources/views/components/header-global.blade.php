22ffd1606b61078ca4bb136e4cadd03ee0d98225
rbverissimo <rverissimo.dev@gmail.com>
2023-11-03 09:26:52 -0300

    Mudanças no CSS do header adicionando keyframes e animações

--- resources/views/components/header-global.blade.php
+++ resources/views/components/header-global.blade.php
@@ -1,18 +1,26 @@
-<header class="header-global">
-    <div class="header-logo">
-        <div class="div-header-logo">
-            <a class="header-link-logo" href="{{route('site.home')}}">Nome do produto</a>
+<!DOCTYPE html>
+<html>
+    <head> 
+    </head>
+    <body>
+    <header class="header-global">
+        <div class="header-logo">
+            <div class="div-header-logo">
+                <a class="header-link-logo" href="{{route('site.home')}}">acordes&cordas</a>
+            </div>
         </div>
-    </div>
-    <div class="container-links-header">
-        <div class="div-header-link">
-            <a class="header-link" href="{{route('site.painel')}}">Blog</a>
+        <div class="container-links-header">
+            <div class="div-header-link">
+                <a class="header-link" href="{{route('site.painel')}}">Blog</a>
+            </div>
+            <div class="div-header-link">
+                <a class="header-link" href="{{route('site.painel')}}">Área do Aluno</a>
+            </div>
+            <div class="div-header-link">
+                <a class="header-link" href="{{route('site.painel')}}">Área do Professor</a>
+            </div>
         </div>
-        <div class="div-header-link">
-            <a class="header-link" href="{{route('site.painel')}}">Área do Aluno</a>
-        </div>
-        <div class="div-header-link">
-            <a class="header-link" href="{{route('site.painel')}}">Área do Professor</a>
-        </div>
-    </div>
-</header>
+    </header>
+    </body>
+
+</html>
