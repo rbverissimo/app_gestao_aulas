900a069137cf4445331f5fb0f8139a6104a9a770
renatoFromRyzen7 <rverissimo.dev@gmail.com>
2023-11-04 14:59:42 -0300

    Adicionando as relações entre os principais models

--- app/Models/Aluno.php
+++ app/Models/Aluno.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Aluno extends Model
 {
@@ -17,5 +18,8 @@ class Aluno extends Model
 
     protected $fillable = ['nome', 'cpf', 'celular', 'idResponsavel'];
 
+    public function responsaveis(): HasMany {
+        return $this->hasMany(Responsavel::class, 'idResponsavel');
+    }
     
 }
--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
 </head>
 <body>
     <div class="container mt-5">
@@ -27,7 +28,7 @@
                             </div>
 
                             <div class="form-group">
-                                <button type="submit" class="btn btn-primary">Login</button>
+                                <button type="submit" class="btn btn-primary custom-button">Login</button>
                             </div>
                         </form>
                     </div>
